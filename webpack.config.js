bf3385446e2558bd53d9ac7747f463039c763fea
luoliyi <mopln510@qq.com>
2019-08-08 19:52:51 +0800

    config webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,20 @@ module.exports = {
   // entry 需要打包的入口文件
   entry: {
     main1: './src/views/webpackfile/demo.js',
-    main2: './src/views/webpackfile/demo2.js'
+    main2: './src/views/webpackfile/demo2.js',
+    txt1: './src/assets/hello.txt',
+    css1: './src/assets/cssdemo.css'
   },
   mode: 'development',
   // 打包后的输出文件
   output: {
     path: path.resolve(__dirname, './dist/hadPack'),
     filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' },
+      { test: /\.css$/, use: 'css-loader' }
+    ]
   }
 }

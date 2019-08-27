11b3d243a965c7c992a0b7d6128987d86e28f995
luoliyi <mopln510@qq.com>
2019-08-27 19:06:45 +0800

    add img-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     main1: './src/views/webpackfile/demo.js',
     main2: './src/views/webpackfile/demo2.js',
     txt1: './src/assets/hello.txt',
-    css1: './src/assets/cssdemo.css'
+    css1: './src/assets/cssdemo.css',
+    img1: './src/assets/car.png'
   },
   mode: 'development',
   // 打包后的输出文件
@@ -16,7 +17,31 @@ module.exports = {
   module: {
     rules: [
       { test: /\.txt$/, use: 'raw-loader' },
-      { test: /\.css$/, use: 'css-loader' }
+      { test: /\.css$/, use: 'css-loader' },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:5].min.[ext]',
+              limit: 10000, // size <= 20KB
+              publicPath: 'assets/',
+              outputPath: 'assets/'
+            }
+          },
+          {
+            loader: 'img-loader',
+            options: {
+              plugins: [
+                require('imagemin-pngquant')({
+                  quality: [0.4, 0.5]
+                })
+              ]
+            }
+          }
+        ]
+      }
     ]
   }
 }
